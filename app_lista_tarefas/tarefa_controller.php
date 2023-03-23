a2c672e320cb8e0bc4192384e20edbb615d18090
Roger Ferreira Da Silva <74381457+fsRoger@users.noreply.github.com>
2023-03-23 14:48:13 -0300

    Add files via upload

--- app_lista_tarefas/tarefa_controller.php
+++ app_lista_tarefas/tarefa_controller.php
@@ -23,6 +23,70 @@
 
       $tarefaService = new TarefaService($conexao, $tarefa);
       $tarefas = $tarefaService->recuperar();
+
+  } else if($acao == 'atualizar') {
+
+    $tarefa = new Tarefa();
+
+    $tarefa->__set('id', $_POST['id']);
+    $tarefa->__set('tarefa', $_POST['tarefa']);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    if($tarefaService->atualizar()) {
+
+      if( isset($_GET['pag']) && $_GET['pag'] == 'index') {
+          header('location: index.php');
+      } else {
+          header('location: todas_tarefas.php');
+      }
+    }
+
+  } else if($acao == 'remover') {
+
+    $tarefa = new Tarefa();
+    $tarefa->__set('id', $_GET['id']);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefaService->remover();
+
+    if( isset($_GET['pag']) && $_GET['pag'] == 'index') {
+        header('location: index.php');
+    } else {
+      header('location: todas_tarefas.php');
+    }
+
+  } else if($acao == 'marcarRealizada') {
+
+    $tarefa = new Tarefa();
+
+    $tarefa->__set('id', $_GET['id']);
+    $tarefa->__set('id_status', 2);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefaService->marcarRealizada();
+
+    if( isset($_GET['pag']) && $_GET['pag'] == 'index') {
+      header('location: index.php');
+    } else {
+      header('location: todas_tarefas.php');
+    }
+
+  } else if($acao == 'recuperarTarefasPendentes') {
+    $tarefa = new Tarefa();
+    $tarefa->__set('id_status', 1);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefas = $tarefaService->recuperarTarefasPendentes();
+
   }
+
  
 ?>
